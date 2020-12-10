de6b0fa4500c0a8b2a44c1d7b53a719065b84c4c
OwenAspen <70032488+OwenAspen@users.noreply.github.com>
2020-12-09 21:48:21 -0500

    Update autoMontyHall.py
    Added counters for number of each experiment done and changed calculation of winning percentages to use those counters instead of approximating the number of wins.

--- autoMontyHall.py
+++ autoMontyHall.py
@@ -1,6 +1,8 @@
 import random
 #The Monty Hall problem.
 numExperiments = 1000000
+firstGuessExperiments = 0
+secondGuessExperiments = 0
 winFirstGuess = 0
 winSecondGuess = 0
 #win counters
@@ -14,10 +16,12 @@ for experimentNum in range(numExperiments):
     reguess = random.randint(0,1)
     #decide if you want to guess again after being shown one incorrect answer.
     if reguess == 0:
+        firstGuessExperiments += 1
         if doors[guess1] == 1:
             winFirstGuess += 1
         #result if not reguessing
     else:
+        secondGuessExperiments += 1
         if doors[0] == 0:
             secondGuess = random.randint(1,2)
             if doors[secondGuess] == 1:
@@ -28,5 +32,5 @@ for experimentNum in range(numExperiments):
                 winSecondGuess +=1
     #result if you guess again.
 print('After running ' + str(numExperiments) + ' simulations the results are:')
-print('The winning percentage using the first guess is ' +str((winFirstGuess/numExperiments)*200)+'%')
-print('The winning percentage using the second guess is ' +str((winSecondGuess/numExperiments)*200)+'%')
+print('The winning percentage using the first guess is ' +str((winFirstGuess/firstGuessExperiments)*100)+'%')
+print('The winning percentage using the second guess is ' +str((winSecondGuess/secondGuessExperiments)*100)+'%')
